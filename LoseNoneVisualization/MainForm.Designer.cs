2dfa872f25d87e66f0f1f230bb3e7c2010e8cef5
XianlitiCN <mitochondriacn@gmail.com>
2020-11-21 14:25:02 +0800

    布局主窗体

--- LoseNoneVisualization/MainForm.Designer.cs
+++ LoseNoneVisualization/MainForm.Designer.cs
@@ -29,13 +29,158 @@ namespace LoseNoneVisualization
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.tblMainLayouter = new System.Windows.Forms.TableLayoutPanel();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblOnlinePlayerNumber = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.chartOnlinePlayers = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.tblMainLayouter.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chartOnlinePlayers)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tblMainLayouter
+            // 
+            this.tblMainLayouter.ColumnCount = 1;
+            this.tblMainLayouter.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tblMainLayouter.Controls.Add(this.lblTitle, 0, 0);
+            this.tblMainLayouter.Controls.Add(this.tableLayoutPanel1, 0, 1);
+            this.tblMainLayouter.Controls.Add(this.tabControl1, 0, 2);
+            this.tblMainLayouter.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tblMainLayouter.Location = new System.Drawing.Point(0, 0);
+            this.tblMainLayouter.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tblMainLayouter.Name = "tblMainLayouter";
+            this.tblMainLayouter.RowCount = 3;
+            this.tblMainLayouter.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tblMainLayouter.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tblMainLayouter.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 28F));
+            this.tblMainLayouter.Size = new System.Drawing.Size(765, 441);
+            this.tblMainLayouter.TabIndex = 0;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("微软雅黑 Light", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblTitle.Location = new System.Drawing.Point(3, 0);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(217, 38);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "LoseNone 实时";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.lblOnlinePlayerNumber, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 41);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(759, 20);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "在线玩家";
+            // 
+            // lblOnlinePlayerNumber
+            // 
+            this.lblOnlinePlayerNumber.AutoSize = true;
+            this.lblOnlinePlayerNumber.Location = new System.Drawing.Point(65, 0);
+            this.lblOnlinePlayerNumber.Name = "lblOnlinePlayerNumber";
+            this.lblOnlinePlayerNumber.Size = new System.Drawing.Size(15, 17);
+            this.lblOnlinePlayerNumber.TabIndex = 1;
+            this.lblOnlinePlayerNumber.Text = "0";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Alignment = System.Windows.Forms.TabAlignment.Bottom;
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(3, 67);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(759, 371);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.chartOnlinePlayers);
+            this.tabPage1.Location = new System.Drawing.Point(4, 4);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(751, 341);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "在线人数";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // chartOnlinePlayers
+            // 
+            chartArea1.Name = "ctaPlayers";
+            this.chartOnlinePlayers.ChartAreas.Add(chartArea1);
+            this.chartOnlinePlayers.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend1.Name = "lgnNormal";
+            legend1.Title = "图例";
+            this.chartOnlinePlayers.Legends.Add(legend1);
+            this.chartOnlinePlayers.Location = new System.Drawing.Point(3, 3);
+            this.chartOnlinePlayers.Name = "chartOnlinePlayers";
+            series1.ChartArea = "ctaPlayers";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Legend = "lgnNormal";
+            series1.LegendText = "在线玩家数";
+            series1.Name = "serOnlinePlayerNumber";
+            this.chartOnlinePlayers.Series.Add(series1);
+            this.chartOnlinePlayers.Size = new System.Drawing.Size(745, 335);
+            this.chartOnlinePlayers.TabIndex = 0;
+            this.chartOnlinePlayers.Text = "chart1";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(765, 441);
+            this.Controls.Add(this.tblMainLayouter);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "MainForm";
+            this.Text = "LoseNone 可视化 - 代号霜";
+            this.tblMainLayouter.ResumeLayout(false);
+            this.tblMainLayouter.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chartOnlinePlayers)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tblMainLayouter;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblOnlinePlayerNumber;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartOnlinePlayers;
     }
 }
 
