261c3f13dd05b6eac0ae8ef6b52f3d19ba007c4b
XianlitiCN <mitochondriacn@gmail.com>
2020-11-22 20:28:21 +0800

    添加移动端项目。

---
+++ LosePanel.Mobile/LosePanel.Mobile/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using LosePanel.Mobile.ViewModels;
+using LosePanel.Mobile.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace LosePanel.Mobile
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ LosePanel.Mobile/LosePanel.Mobile/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using LosePanel.Mobile.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace LosePanel.Mobile.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
