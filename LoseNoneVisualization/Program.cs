00c28e968aa56f112ba930b6d771d0f182880e53
线粒体XianlitiCN <mitochondriacn@gmail.com>
2021-03-06 13:12:21 +0800

    结构修改。

--- LoseNoneVisualization/Program.cs
+++ LoseNoneVisualization/Program.cs
@@ -4,7 +4,6 @@ using System.Drawing.Text;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using LosePanel.Forms;
 
 namespace LosePanel
 {
@@ -18,18 +17,7 @@ namespace LosePanel
         [STAThread]
         static void Main()
         {
-            byte[] fontData = Properties.Resources.FZYH;
-            //打开"允许不安全代码编译"开关，此句才不报错 
-            unsafe
-            {
-                fixed (byte* pFontData = fontData)
-                {
-                    CustomFont.AddMemoryFont((System.IntPtr)pFontData, fontData.Length);
-                }
-            }
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            
         }
     }
 }
