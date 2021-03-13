1e990d51bfcfe25ff1684d3e098425ba51066bcd
线粒体XianlitiCN <mitochondriacn@gmail.com>
2021-03-13 19:28:47 +0800

    更改发布属性。

--- LosePanel.SDK/Properties/AssemblyInfo.cs
+++ LosePanel.SDK/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("LosePanel.SDK")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("洛书南 & 线粒体")]
+[assembly: AssemblyCompany("洛书南")]
 [assembly: AssemblyProduct("LosePanel.SDK")]
-[assembly: AssemblyCopyright("Copyright © 2020 LoseNone, XianlitiCN")]
+[assembly: AssemblyCopyright("Copyright © 2020 洛书南")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
 //通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.0.0")]
-[assembly: AssemblyFileVersion("0.2.0.0")]
+[assembly: AssemblyVersion("0.4.0.0")]
+[assembly: AssemblyFileVersion("0.4.0.0")]
